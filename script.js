0108b7c91016105099fa6131e6a6224c26a0b0ce
Samuel Back <h22samub@du.se>
2025-12-30 14:45:59 +0100

    Duglig html + början av script.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//Projekt Dynamiska Webbapplikationer Grupp 6 
+//Projekt Dynamiska Webbapplikationer Grupp 6
+const textfields = document.getElementsByName('textfield')
+const submit = document.getElementById('submit');
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', (e) => {
+
+for (let i = 0; i < textfields.length; i++) {
+    textfields[i].value = '';
+}
+})
